380d95d481522b4aa325e8f69a6145ead64fa0b8
advaneharshal <31953365+advaneharshal@users.noreply.github.com>
2023-02-12 22:22:21 +0530

    Update logic_analyser.v
    Updated - Fixed the issues

--- logic_analyser.v
+++ logic_analyser.v
@@ -4,6 +4,10 @@
 `define STATE2 ADDR
 `define STATE3 EOF
 `define STATE_DEFAULT DEFAULT
+`define STATE0_BITS 1
+`define STATE1_BITS 8
+`define STATE2_BITS 4
+`define STATE3_BITS 2
 module top();
 parameter NUM_STATES = 4;
 parameter NUM_INPUTS = 1;
@@ -29,7 +33,8 @@ end
 //output [NUM_STATES-1:0] outputs;
 reg [NUM_INPUTS-1:0] inputs;
 bit [31:0] out;
-
+bit [31:0] result;
+bit [31:0] final_result;
 // define the state and next state registers
 typedef enum bit [127:0] {`STATE_DEFAULT=0,`STATE0=1,`STATE1=2,`STATE2=3,`STATE3=4} fsm_state_e;
 //reg [NUM_STATES-1:0] state, next_state;
@@ -41,7 +46,7 @@ begin
    case(state)
       
       `STATE0: begin
-         repeat(1)  begin
+         repeat(`STATE0_BITS)  begin
             out  = out | in<<i;
             $display ($time," Inside %s val out:%0b %h in:%0b",`STATE0,out,out,in);
             i++;
@@ -53,40 +58,50 @@ begin
       `STATE1: begin
          i=0;
          $display ($time," Inside state1");
-         repeat(8)  begin
-            out[0:7]  = out[0:7] | in<<i; 
+         repeat(`STATE1_BITS)  begin
+            $display ("dbg0: out %b in %b i %0d ",out,in,i);
+            out  =  in<<i| out ; 
+            result[`STATE1_BITS-1:0] = {<<{out[`STATE1_BITS-1:0]}}; 
+            $display ("dbg1: out %b in %b i %0d ",out,in,i);
+            if(i ==`STATE1_BITS-1) final_result <= result;
             $display ($time," Inside %s val out:%0b %h in:%0b",`STATE1,out,out,in);
             @(posedge clk);
             i++;
          end
          $display (" changing the state ");
          next_state = `STATE2;
-         out = 0;i=0;
+         out = 0;i=0;result = 0;final_result =0;
       end
       `STATE2: begin
          i=0;
          $display ($time," Inside state2");
-         repeat(4) begin
-            out[0:3]  = out[0:3] | in<<i; 
+         repeat(`STATE2_BITS) begin
+           // $display ("dbg0: out %b in %b i %0d ",out,in,i);
+            out  =  in<<i| out ;
+            result[`STATE2_BITS-1:0] = {<<{out[`STATE2_BITS-1:0]}}; 
+           // $display ("dbg1: out %b in %b i %0d ",out,in,i);
+            if(i ==`STATE2_BITS-1) final_result <= result;
             $display ($time," Inside %s val out:%0b %h in:%0b",`STATE2,out,out,in);
             @(posedge clk);
             i++;
          end
          $display (" changing the state ");
          next_state = `STATE3;
-         out = 0;i=0;
+         out = 0;i=0;result = 0;final_result =0;
       end
       `STATE3: begin
          i=0;
          $display ($time," Inside state3");
-         repeat(2)  begin
+         repeat(`STATE3_BITS)  begin
             out  = out | in<<i; 
+            result[`STATE3_BITS-1:0] = {<<{out[`STATE3_BITS-1:0]}}; 
+            if(i ==`STATE3_BITS-1) final_result <= result;
             $display ($time," Inside %s val out:%0b %h in:%0b",`STATE3,out,out,in);
             @(posedge clk);
             i++;
          end
          next_state = `STATE_DEFAULT;
-         out = 0;i=0;
+         out = 0;i=0;result = 0;final_result =0;
          start = 0;
       end
 
